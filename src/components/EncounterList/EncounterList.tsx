756589733bd5779dbdcb624ccc17bc24418805fa
Luke Jacobsen <lukejacobsen@gmail.com>
2024-04-16 08:47:32 +0200

    added ability to add

--- src/components/EncounterList/EncounterList.tsx
+++ src/components/EncounterList/EncounterList.tsx
@@ -3,6 +3,7 @@ import { useLocalStorage } from "usehooks-ts";
 import { ICreature } from "../Creature/Creature";
 import { EmptyState } from "./EmptyState";
 import { EncounterItem } from "../EncounterItem/EncounterItem";
+import { useNavigate } from "react-router-dom";
 
 interface Encounter {
   creatures: ICreature[];
@@ -14,6 +15,7 @@ interface Encounter {
 
 export const EncounterList = () => {
   const [creatureList] = useLocalStorage<ICreature[]>("encounter", []);
+  const navigate = useNavigate();
 
   const importedList: Encounter[] =
     creatureList.length > 0
@@ -43,6 +45,19 @@ export const EncounterList = () => {
     }
   };
 
+  const handleOnCreate = (newName: string) => {
+    const newEncounter: Encounter = {
+      id: crypto.randomUUID(),
+      name: newName,
+      creatures: [],
+      createdOn: new Date().toISOString(),
+      lastUpdatedOn: new Date().toISOString(),
+    };
+
+    setEncounterList([...encounterList, newEncounter]);
+    navigate(newEncounter.id);
+  };
+
   return (
     <Container sx={{ px: 2, pt: 9, pb: 8 }}>
       {encounterList.length > 0 ? (
@@ -57,7 +72,7 @@ export const EncounterList = () => {
           ))}
         </Stack>
       ) : (
-        <EmptyState />
+        <EmptyState onCreate={handleOnCreate} />
       )}
     </Container>
   );
