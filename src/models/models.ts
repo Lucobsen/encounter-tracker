871f97764994b4648e07f02735af96ca6c919c38
Luke Jacobsen <lukejacobsen@gmail.com>
2024-07-16 21:59:33 +0200

    fix letter bug and dep warning

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,14 +1,14 @@
 export const letterMapping: Record<number, string> = {
-  1: "A",
-  2: "B",
-  3: "C",
-  4: "D",
-  5: "E",
-  6: "F",
-  7: "G",
-  8: "H",
-  9: "I",
-  10: "J",
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+  7: "H",
+  8: "I",
+  9: "J",
 };
 
 export const quantities: { value: number; label: string }[] = [
